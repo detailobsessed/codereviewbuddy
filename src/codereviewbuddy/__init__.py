9ca0c7cbd44a7ff16da06e425f384f8659433dc0
Ismar Iljazovic <ismar@gmail.com>
2026-02-09 00:26:10 +0100

    build: copier update from ci-components template

--- src/codereviewbuddy/__init__.py
+++ src/codereviewbuddy/__init__.py
@@ -1,8 +1,1 @@
-"""codereviewbuddy.
-
-codereview buddy helps your AI agent interact with AI code review--smoothly!
-"""
-
-from __future__ import annotations
-
-__all__: list[str] = []
+"""codereviewbuddy."""
